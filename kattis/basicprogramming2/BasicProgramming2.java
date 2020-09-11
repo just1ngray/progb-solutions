c616b0613b8f84ff582edaee3af276ce80c14a73
just1ngray <just1ngray@outlook.com>
2020-09-11 00:00:26 -0300

    added Kattio utility to improve input reading speed

--- kattis/basicprogramming2/BasicProgramming2.java
+++ kattis/basicprogramming2/BasicProgramming2.java
@@ -6,23 +6,30 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.Map;
+
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
 
 public class BasicProgramming2 {
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Kattio io = new Kattio(System.in, System.out);
 
-        int arraySize = sc.nextInt();
-        int taskCode = sc.nextInt(); sc.nextLine();
+        int arraySize = io.getInt();
+        int taskCode = io.getInt();
 
         ArrayList<Integer> array = new ArrayList<>(arraySize);
         for (int i = 0; i < arraySize; i ++) {
-            array.add(sc.nextInt());
+            array.add(io.getInt());
         }
-        sc.close();
 
         switch (taskCode) {
             case 1:
@@ -117,4 +124,87 @@ public class BasicProgramming2 {
         System.out.println(sb);
     }
 
+}
+
+/** Simple yet moderately fast I/O routines.
+ *
+ * Example usage:
+ *
+ * Kattio io = new Kattio(System.in, System.out);
+ *
+ * while (io.hasMoreTokens()) {
+ *    int n = io.getInt();
+ *    double d = io.getDouble();
+ *    double ans = d*n;
+ *
+ *    io.println("Answer: " + ans);
+ * }
+ *
+ * io.close();
+ *
+ *
+ * Some notes:
+ *
+ * - When done, you should always do io.close() or io.flush() on the
+ *   Kattio-instance, otherwise, you may lose output.
+ *
+ * - The getInt(), getDouble(), and getLong() methods will throw an
+ *   exception if there is no more data in the input, so it is generally
+ *   a good idea to use hasMoreTokens() to check for end-of-file.
+ *
+ * @author: Kattis
+ */
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) { }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
 }
